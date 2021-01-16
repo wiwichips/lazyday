28fe9a6bf1f6caff98846d6d5375349a67a58508
wiwichips <wpringle@uoguelph.ca>
2021-01-16 15:01:08 -0500

    Add js to read input from settings

---
+++ settings.js
@@ -0,0 +1,25 @@
+console.log('hello world from settings.js');
+
+// Put all onload AJAX calls here, and event listeners
+$(document).ready(() => {
+
+  // listen for login submission
+  $('#submit-button').submit((e) => {
+    e.preventDefault();
+
+    const form = document.getElementById('ib-first-name');
+
+    console.log(form); 
+  });
+
+  $('#informationCenter').submit((e) => {
+    e.preventDefault();
+    console.log('button pressed');
+    const form = document.getElementById('informationCenter');
+    const firstname = form.ibfirstname.value;
+    const middlename = form.ibmiddlename.value;
+    console.log(firstname);
+    console.log(middlename);
+    console.log(form);
+  });
+});
