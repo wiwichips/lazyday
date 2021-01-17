b9e186ab2a26d3cb2285cd1e8f8b5cb0026ac7c9
jatep <janefivesup@hotmail.com>
2021-01-17 01:54:07 -0500

    Add Fields Box

--- settings.js
+++ settings.js
@@ -81,16 +81,16 @@ $(document).ready(() => {
 });
 
 $(document).ready(function() {
-	var max_fields      = 10;
+	var max_fields      = 16;
 	var wrapper   		= $(".input_fields_wrap");
 	var add_button      = $(".add_field_button");
 	
-	var x = 1;
+	var x = 0;
 	$(add_button).click(function(e){
 		e.preventDefault();
 		if(x < max_fields){ 
-			x++; 
-			$(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+			x++ +4; 
+			$(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field ">Remove</a></div>'); 
 		}
 	});
 	
