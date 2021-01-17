3c50b62492b5aa17c4bcc96ed3f1c5609cf73fa4
wiwichips <wpringle@uoguelph.ca>
2021-01-17 06:50:05 -0500

    works on load not when btn pressed

--- popup.js
+++ popup.js
@@ -9,4 +9,19 @@ window.addEventListener('load', function (evt) {
 chrome.runtime.onMessage.addListener(function (message) {
 	console.log(message);
 	document.getElementById('pagetitle').innerHTML = message;
+	console.log('hellow orld');
 });
+
+function onButtonClicked (){
+  console.log('btnComment worked');
+}
+
+document.getElementById('autoFillBtn').addEventListener('click', onButtonClicked());
+
+// chrome.runtime.onMessage.addListener(function(message, callback) {
+// 	if (message == "runContentScript"){
+// 		chrome.tabs.executeScript({
+// 			file: 'payload.js'
+// 		});
+// 	}
+// });
