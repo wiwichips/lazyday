6fc85fa9f605518bfcac0681d5ad1193df76254d
wiwichips <wpringle@uoguelph.ca>
2021-01-17 03:22:20 -0500

    Change

--- payload.js
+++ payload.js
@@ -1,5 +1,96 @@
-// send the page title as a chrome message
+const obj = document.querySelectorAll('input[id^="input-"]');
+let str = null;
+
+const exampleUserData = {
+  firstname: 'Will',
+  middlename: 'Kantor',
+  lastname: 'Pringle',
+  address: '200 XXX st',
+  city: 'Kingston',
+  postalCode: 'XXXXXX',
+  phoneNumber: '3331114444',
+  phoneExtension: '',
+  jobTitle: [],
+  jobCompany: [],
+  jobLocation: [],
+  school: 'University of Guelph',
+  gpa: '3.68',
+  socialWebsite: '',
+  socialLinkedIn: 'https://www.linkedin.com/in/will-pringle/',
+}
+
+console.log(document.title);
+
+for (let i = 0; i < obj.length; i++) {
+  str = obj[i].outerHTML;
+  str = str.slice(str.indexOf('data-automation-id'), str.length);
+  str = str.slice(str.indexOf('\"') + 1, str.length);
+  str = str.slice(0, str.indexOf('\"'));
+  console.log(str);
+
+  enterUserData(str, exampleUserData, obj[i]);
+}
+
+console.log(obj.length);
+
+function enterUserData(type, userData, element) {
+  if (type === 'legalNameSection_firstName') {
+    element.value = exampleUserData.firstname;
+  }
+  else if (type === 'legalNameSection_middleName') {
+    element.value = exampleUserData.middlename;
+  }
+  else if (type === 'legalNameSection_lastName') {
+    element.value = exampleUserData.lastname;
+  }
+  else if (type === 'addressSection_addressLine1') {
+    element.value = exampleUserData.address;
+  }
+  else if (type === 'addressSection_city') {
+    element.value = exampleUserData.city;
+  }
+  else if (type === 'phone-number') {
+    element.value = exampleUserData.address;
+  }
+  else if (type === 'phone-extension') {
+    element.value = exampleUserData.address;
+  }
+  else if (
+    type === 'jobTitle' || type === 'company' || type === 'location' 
+    ) {
+    console.log('job');
+  }
+  else if (type === 'school') {
+    element.value = exampleUserData.school;
+  }
+  else if (type === 'gpa') {
+    element.value = exampleUserData.gpa;
+  }
+  else if (type === 'website') {
+    element.value = exampleUserData.socialWebsite;
+  }
+  else if (type === 'linkedinQuestion') {
+    element.value = exampleUserData.socialLinkedIn;
+  }
+}
 
 
-
+// send the page title as a  message
 chrome.runtime.sendMessage(document.title);
+
+/*
+// Here is a list of attributes that are checked for in workday
+
+legalNameSection_firstName
+legalNameSection_middleName
+legalNameSection_lastName
+addressSection_addressLine1
+addressSection_city
+addressSection_postalCode
+phone-number
+phone-extension
+school
+gpa
+website
+linkedinQuestion
+*/
--- settings.js
+++ settings.js
@@ -13,88 +13,117 @@ $(document).ready(() => {
   });
 
   $('#informationCenter').submit((e) => {
-    e.preventDefault();
-    console.log('button pressed');
-    const form = document.getElementById('informationCenter');
-    const firstname = form.ibfirstname.value;
-    const middlename = form.ibmiddlename.value;
-	const lastname = form.iblastname.value;
-	const address = form.ibaddress.value;
-    const city = form.ibcity.value;
-	const province = form.ibprovince.value;
-	const postalcode = form.ibpostalcode.value;
-	const phonecode = form.ibphonecode.value;
-	const phonenumber = form.ibphonenumber.value;
-	const phoneext = form.ibphoneext.value;
-	const jobtitle = form.ibjobtitle.value;
-	const company = form.ibcompany.value;
-	const joblocation = form.ibjoblocation.value;
-	const roledescription = form.ibroledescription.value;
-	const school = form.ibschool.value;
-	const field = form.ibfield.value;
-	const gpa = form.ibgpa.value;
-	const certific = form.ibcertific.value;
-	const certificnum = form.ibcertificnum.value;
-	const specialty = form.ibspecialty.value;
-	const skills = form.ibskills.value;
-	const resumefile = form.ibresumefile.value;
-	const linkedin = form.iblinkedin.value;
-	const twitter = form.ibtwitter.value;
-	const google = form.ibgoogle.value;
-	const facebook = form.ibfacebook.value;
-	const name = form.ibname.value;
-	const employeeid = form.ibemployeeid.value;
-	//const  = form.ib.value;
+		e.preventDefault();
+		console.log('button pressed');
+		const form = document.getElementById('informationCenter');
+		const firstname = form.ibfirstname.value;
+		const middlename = form.ibmiddlename.value;
+		const lastname = form.iblastname.value;
+		const address = form.ibaddress.value;
+		const city = form.ibcity.value;
+		const province = form.ibprovince.value;
+		const postalcode = form.ibpostalcode.value;
+		const phonecode = form.ibphonecode.value;
+		const phonenumber = form.ibphonenumber.value;
+		const phoneext = form.ibphoneext.value;
+		const jobtitle = form.ibjobtitle.value;
+		const company = form.ibcompany.value;
+		const joblocation = form.ibjoblocation.value;
+		const roledescription = form.ibroledescription.value;
+		const school = form.ibschool.value;
+		const field = form.ibfield.value;
+		const gpa = form.ibgpa.value;
+		const certific = form.ibcertific.value;
+		const certificnum = form.ibcertificnum.value;
+		const specialty = form.ibspecialty.value;
+		const skills = form.ibskills.value;
+		const resumefile = form.ibresumefile.value;
+		const linkedin = form.iblinkedin.value;
+		const twitter = form.ibtwitter.value;
+		const google = form.ibgoogle.value;
+		const facebook = form.ibfacebook.value;
+		const name = form.ibname.value;
+		const employeeid = form.ibemployeeid.value;
+		//const  = form.ib.value;
 
-    console.log(firstname);
-    console.log(middlename);
-	console.log(lastname);
-	console.log(address);
-    console.log(city);
-	console.log(province);
-	console.log(postalcode);
-	console.log(phonecode);
-	console.log(phonenumber);
-	console.log(phoneext);
-	console.log(jobtitle);
-	console.log(company);
-	console.log(joblocation);
-	console.log(roledescription)
-	console.log(school);
-	console.log(field);
-	console.log(gpa);
-	console.log(certific);
-	console.log(certificnum);
-	console.log(specialty);
-	console.log(skills);
-	console.log(resumefile);
-	console.log(linkedin);
-	console.log(twitter);
-	console.log(google);
-	console.log(facebook);
-	console.log(name);
-	console.log(employeeid);
-	//console.log();
+		const jobtitle2 = 'XXX - NOT ASSIGNED TO ANYTHING YET';
+		const company2 = 'XXX - NOT ASSIGNED TO ANYTHING YET';
+		const joblocation2 = 'XXX - NOT ASSIGNED TO ANYTHING YET';
+		const roledescription2 = 'XXX - NOT ASSIGNED TO ANYTHING YET';
+
+		const exampleUserData = {
+			firstname: firstname,
+			middlename: middlename,
+			lastname: lastname,
+			address: address,
+			city: city,
+			province: province,
+			postalCode: postalcode,
+			phoneNumber: phonenumber,
+			phoneCode: phonecode,
+			phoneExtension: phoneext,
+			jobTitle: [
+				jobtitle,
+				jobtitle2,
+			],
+			jobCompany: [
+				company,
+				company2,
+			],
+			jobLocation: [
+				joblocation,
+				joblocation2,
+			],
+			roledescription: [
+				roledescription,
+				roledescription2,
+			],
+			school: school,
+			field: field,
+			gpa: gpa,
+			certific: certific,
+			certificnum: certificnum,
+			specialty: specialty,
+			skills: skills,
+			resumefile: resumefile,
+			socialWebsite: '',
+			socialLinkedIn: linkedin,
+		}
+
+		console.log(twitter);
+		console.log(google);
+		console.log(facebook);
+		console.log(name);
+		console.log(employeeid);
+		//console.log();
 	
     console.log(form);
-  });
-});
-
-$(document).ready(function() {
-	var max_fields      = 16;
-	var wrapper   		= $(".input_fields_wrap");
-	var add_button      = $(".add_field_button");
-	
-	var x = 0;
-	$(add_button).click(function(e){
-		e.preventDefault();
-		if(x < max_fields){ 
-			x++ +4; 
-			$(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field ">Remove</a></div>'); 
-		}
 	});
 	
-	$(wrapper).on("click",".remove_field", function(e){ 
-		e.preventDefault(); $(this).parent('div').remove(); x--;
-	})
+	$('#add_button').submit((e) => {
+    e.preventDefault();
+
+    console.log('ADD BUTTON WAS JUST PRESSED');
+  });
+
+
 });
+
+// $(document).ready(function() {
+// 	var max_fields      = 16;
+// 	var wrapper   		= $(".input_fields_wrap");
+// 	var add_button      = $(".add_field_button");
+	
+// 	var x = 0;
+// 	$(add_button).click(function(e){
+// 		e.preventDefault();
+// 		if(x < max_fielfds){ 
+// 			x++ +4; 
+// 			$(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field ">Remove</a></div>'); 
+// 		}
+// 	});
+	
+// 	$(wrapper).on("click",".remove_field", function(e){ 
+// 		e.preventDefault(); $(this).parent('div').remove(); x--;
+// 	})
+// });
